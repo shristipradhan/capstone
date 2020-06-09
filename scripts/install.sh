2d9b3948ebb4ace87bc102445288bda23806fbe9
Shristi Pradhan <shristi.pr@gmail.com>
2020-06-08 21:21:44 -0700

    Update install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+rm -rf ~/capstone
+mkdir -p ~/capstone/src
+cd ~/capstone/src
+git clone --single-branch --branch release https://github.com/shristipradhan/capstone.git
+cd capstone
+mkdir -p  ../build
+cd ../build
+cmake ../capstone
+make -j$(nproc)
+./DisplayImage
